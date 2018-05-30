c34891cb33831af2c00ed81d40e4f736d6fccec5
bdurb <brian@rpmgreenville.com>
2018-05-30 14:47:54 -0700

    wrapped up article page, and added gsap to head of index.html

--- Article/Article.js
+++ Article/Article.js
@@ -9,9 +9,9 @@ class Article {
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.innerText = 'expand';
     // Set a click handler on the expandButton reference (or article element), calling the expandArticle method.
-    this.expandButton.addEventListner('click', this.expandArticle);
+    this.expandButton.addEventListener('click', () => {this.expandArticle()} );
   }
-
+  
   expandArticle() {
     // Using our reference to the article element, add or remove a class to expand or hide the article.
     this.element.classList.toggle('article-open');
@@ -22,5 +22,5 @@ class Article {
 let articles = document.querySelectorAll('.article');
 
 // Use .map() to iterate over the articles array and create a new instance of Article by passing in each article element as a parameter to the constructor.
-articles = Array.from(articles).map( article => new Article(article));
+articles = Array.from(articles).map(article => new Article(article));
 
